9d929eb706d8c99c54123a0fa6d192253c376516
Madhurendra Tripathy <42348233+MadhurendraTripathy@users.noreply.github.com>
2020-04-20 16:44:42 +0530

    Create Day-3_MaximumSubarray.cpp

---
+++ Day-3_MaximumSubarray.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int max=INT_MIN,sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            if(sum>=max){
+                max=sum;
+                //cout<<max;
+            }
+            if(sum<0){
+                sum=0;   
+            }
+        }
+        return max;
+    }
+};
+
+//driver code
+int main(){
+    ios_base::sync_with_stdio();
+    cin.tie(NULL);
+    cout.tie(NULL);
+    Solution s;
+    vector<int>v = {-2,1,-3,4,-1,2,1,-5,4};
+    cout<<s.maxSubArray(v)<<"\n";
+    return 0;
+}
