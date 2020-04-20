3b8795276296d28794db90362e498d19327bd15b
Madhurendra Tripathy <42348233+MadhurendraTripathy@users.noreply.github.com>
2020-04-20 19:22:44 +0530

    Create Day-10_Min Stack.cpp

---
+++ Day-10_Min Stack.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class MinStack {
+public:
+    int TOP;
+    vector<int>stk;
+    vector<int> min;
+    MinStack() {
+        
+    }
+    
+    void push(int x) {
+        stk.push_back(x);
+        if(min.empty() || x<=min.back() ){
+            min.push_back(x);
+        }
+    }
+    
+    void pop() {
+        if(stk.back()==min.back()){
+            min.pop_back();
+        }
+        stk.pop_back();
+    }
+    
+    int top() {
+        if(!stk.empty())
+            return stk.back();
+        return -1;
+    }
+    
+    int getMin() {
+        if(!min.empty())
+            return min.back();
+        return -1;
+    }
+};
+
+//driver code;
+int main(){
+    MinStack* obj = new MinStack();
+    obj->push(-2);
+    obj->push(0);
+    obj->push(-3);
+    cout<<obj->getMin()<<"\n";
+    obj->pop();
+    obj->top();
+    cout<<obj->getMin()<<"\n";
+    return 0;
+}
