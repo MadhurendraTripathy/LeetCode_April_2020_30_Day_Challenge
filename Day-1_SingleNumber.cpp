996ec36ee82e2b2e768011802042e2340530a1a3
Madhurendra Tripathy <42348233+MadhurendraTripathy@users.noreply.github.com>
2020-04-20 16:37:47 +0530

    Create Day-1_SingleNumber.cpp

---
+++ Day-1_SingleNumber.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int exor = nums[0];
+        for(int i=1;i<(int)nums.size();i++){
+            exor^=nums[i];
+        }
+        return exor;
+    }
+};
+//driver code
+int main(){
+	ios_base::sync_with_stdio();//faster io
+  cin.tie(NULL);
+  cout.tie(NULL);
+  Solution s;
+	vector<int>nums = {4,1,2,1,2};
+	cout<<s.singleNumber(v)<<"\n";
+	return 0;
+}
