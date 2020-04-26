fcad9d29aad3416a6b46dcaacf0d283ac8d74067
Madhurendra Tripathy <42348233+MadhurendraTripathy@users.noreply.github.com>
2020-04-26 13:33:51 +0530

    Create Day-25_JumpGame.cpp

---
+++ Day-25_JumpGame.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool canJump(vector<int>& v) {
+        int n = (int)v.size();
+        int max_location = 0;
+        for(int i=0;i<n;i++){
+            max_location = max(max_location,(i+v[i]));
+            if(max_location == i) break;
+        }
+        return max_location>=n-1;
+    }
+};
+
+int main(){
+    FAST_IO;
+    vector<int>v = {2,3,1,1,4};
+    Solution s;
+    s.canJump(v)?cout<<"YES\n":cout<<"NO\n";
+    return 0;
+}
