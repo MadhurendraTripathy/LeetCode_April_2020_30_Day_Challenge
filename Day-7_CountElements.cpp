e3da4b231251f856c0f8d0047278916e17215623
Madhurendra Tripathy <42348233+MadhurendraTripathy@users.noreply.github.com>
2020-04-28 14:37:07 +0530

    Update Day-7_CountElements.cpp

--- Day-7_CountElements.cpp
+++ Day-7_CountElements.cpp
@@ -3,45 +3,6 @@ using namespace std;
 
 class Solution {
 public:
-
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> v;
-        
-        if(strs.size() == 0) return v;
-        
-        map<string,vector<string>> dict;
-        for(string s: strs){
-            string sorted = s;
-            sort(sorted.begin(),sorted.end());
-            dict[sorted].push_back(s);
-        }
-        map<string,vector<string>>::iterator it;
-        for(it=dict.begin();it!=dict.end();it++){
-            v.push_back(it->second);
-        }
-        return v;
-    }
-};
-
-//driver code
-int main(){
-    ios_base::sync_with_stdio();
-    cin.tie(NULL);
-    cout.tie(NULL);
-    Solution s;
-    vector<strings> v = {"eat", "tea", "tan", "ate", "nat", "bat"};
-    vector<string> ans = s.groupAnagrams(v);
-    for(auto x : ans){
-      for(auto y : x){
-      cout<<y<<" ";
-      }
-    cout<<"\n";
-    }
-    return 0;
-}
-
-class Solution {
-public:
     int countElements(vector<int>& arr) {
         map<int,int>mp;
         for(int num:arr){
@@ -63,15 +24,13 @@ public:
         return count;
     }
 };
-
 //driver code
 int main(){
-    ios_base::sync_with_stdio();
+    ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     Solution s;
-    vector<int> v = {1,3,2,3,5,0};
+    vector<int> v = {1,1,3,3,5,5,7,7};
     cout<<s.countElements(v)<<"\n";
     return 0;
 }
-
